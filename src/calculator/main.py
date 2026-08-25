2e307470ce87292d821d71896be79b46bb0430cc
faoline1-ctrl <faoline1@gmail.com>
2026-08-25 16:22:24 -0600

    A: project set up environment

---
+++ src/calculator/main.py
@@ -0,0 +1,27 @@
+from .operations import *
+
+def main():
+    operation_input = input("What would you like to do? (add, subtract, multiply, divide): ")
+
+    if operation_input == "add":
+        num1 = input("Enter first number\n")
+        num2 = input("Enter second number\n")
+        print(add(int(num1), int(num2)))
+    elif operation_input == "subtract":
+        num1 = input("Enter first number\n")
+        num2 = input("Enter second number\n")
+        print(subtract(int(num1), int(num2)))
+    elif operation_input == "multiply":
+        num1 = input("Enter first number\n")
+        num2 = input("Enter second number\n")
+        print(multiply(int(num1), int(num2)))
+    elif operation_input == "divide":
+        num1 = input("Enter first number\n")
+        num2 = input("Enter second number\n")
+        print(divide(int(num1), int(num2)))
+    else:
+        return "Invalid input"
+
+
+if __name__ == "__main__":
+    main()
